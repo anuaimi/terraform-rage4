2c09b578e4b1525ea51b57963f5f459b538cf170
Athir Nuaimi <athir@nuaimi.com>
2015-11-10 13:32:15 -0500

    fix issues with how priority defined

--- resource_rage4_record.go
+++ resource_rage4_record.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-  // "github.com/anuaimi/rage4"
+  "fmt"
+  "log"
+  "github.com/anuaimi/rage4"
   "github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -13,56 +15,74 @@ func resourceRage4Record() *schema.Resource {
     Delete: resourceRage4RecordDelete,
 
     Schema: map[string]*schema.Schema{
-      "id": &schema.Schema{
-        Type:     schema.TypeString,
+      "domainId": &schema.Schema{
+        Type:     schema.TypeInt,
         Required: true,
       },
-
-
-      "domain": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-        ForceNew: true,
-      },
-
-
       "name": &schema.Schema{
         Type:     schema.TypeString,
         Required: true,
       },
-
-      "hostname": &schema.Schema{
+      "content": &schema.Schema{
         Type:     schema.TypeString,
-        Computed: true,
+        Required: true,
       },
-
       "type": &schema.Schema{
         Type:     schema.TypeString,
         Required: true,
-        ForceNew: true,
       },
-
-      "value": &schema.Schema{
+      "priority": &schema.Schema{
+        Type:     schema.TypeInt,
+        Optional: true,
+        Default:  1,
+      },
+      "active": &schema.Schema{
+        Type:     schema.TypeBool,
+        Optional: true,
+        Default:  true,
+      },
+      "failover": &schema.Schema{
+        Type:     schema.TypeBool,
+        Optional: true,
+        Default:  false,
+      },
+      "failovercontent": &schema.Schema{
         Type:     schema.TypeString,
-        Required: true,
+        Optional: true,
       },
-
+      "failoverwithdraw": &schema.Schema{
+        Type:     schema.TypeString,
+        Optional: true,
+      },
       "ttl": &schema.Schema{
         Type:     schema.TypeString,
         Optional: true,
         Default:  "3600",
       },
 
-      "priority": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
     },
   }
 }
 
 func resourceRage4RecordCreate(d *schema.ResourceData, meta interface{}) error {
-  // client := meta.(*rage4.Client)
+
+  domainId := d.Get("id").(int)
+  newRecord := new(rage4.Record)
+  // fill fields in record
+
+  log.Printf("[DEBUG] Rage4 Record create configuration: %#v", newRecord)
+
+  client := meta.(*rage4.Client)
+  _, err := client.CreateRecord( domainId, *newRecord)
+  if (err != nil) {
+    return fmt.Errorf("Failed to create Rage4 Record: %s", err)
+  }
+
+  // need to extract record id & save
+  recId := 1
+  d.SetId(string(recId))
+  log.Printf("[INFO] record ID: %s", d.Id())
+
   return nil
 }
 
@@ -73,10 +93,22 @@ func resourceRage4RecordRead(d *schema.ResourceData, meta interface{}) error {
 
 func resourceRage4RecordUpdate(d *schema.ResourceData, meta interface{}) error {
   // client := meta.(*rage4.Client)
+  // get id of record to change, 
+  // UpdateRecord( recordId int, record Record) (status Status, err error) {
+
   return nil
 }
 
 func resourceRage4RecordDelete(d *schema.ResourceData, meta interface{}) error {
-  // client := meta.(*rage4.Client)
-  return nil
+  // get which domain we're working on
+  domainId := d.Get("id").(int)
+  recordId := 1
+
+  log.Printf("[INFO] Deleting Rage4 Record for %d in domain %s", d.Get(""), domainId)
+
+  // ask rage4 to delete record
+  client := meta.(*rage4.Client)
+  _,  err := client.DeleteRecord( recordId)
+
+  return err
 }
--- sample.tf
+++ sample.tf
@@ -1,16 +1,16 @@
 # configure provider
 provider "rage4" {
-  "email" = "whatever_used_to_login_to_rage4"
-  "api_key" = "get_account_page_on_webui"
+  email = "whatever_used_to_login_to_rage4"
+  api_key = "get_account_page_on_webui"
 }
 
 # add www record to a domain
 resource "rage4_record" "www" {
-  "zone_id" = ""
-  "name" = ""
-  "content" = ""
-  "type" = ""
-  "priority" = 1
-  "active" = "true"
+  domainId = "1"
+  name = "www"
+  content = "192.168.1.1"
+  type = "A"
+  priority = 1
+  active = "true"
 
 }
