94c6d01b7656c5bcc9b0087f096523a62a128a7a
Athir Nuaimi <athir@nuaimi.com>
2015-11-11 22:37:28 -0500

    code complete on CRUD functions for resource record

--- resource_rage4_record.go
+++ resource_rage4_record.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "log"
+  "strconv"
   "github.com/anuaimi/rage4"
   "github.com/hashicorp/terraform/helper/schema"
 )
@@ -66,49 +67,76 @@ func resourceRage4Record() *schema.Resource {
 
 func resourceRage4RecordCreate(d *schema.ResourceData, meta interface{}) error {
 
-  domainId := d.Get("id").(int)
+  // get id of domain we are going to add server to
+  domainId, err := strconv.Atoi(d.Get("domainId").(string))
+
+  // create new A record
   newRecord := new(rage4.Record)
-  // fill fields in record
+  newRecord.Name = d.Get("name").(string)
+  newRecord.Content = d.Get("content").(string)
+  newRecord.Type = "A"
+  // newRecord.TTL = d.Get("ttl")
 
   log.Printf("[DEBUG] Rage4 Record create configuration: %#v", newRecord)
 
+  // ask Rage4 to add A record to domain
   client := meta.(*rage4.Client)
-  _, err := client.CreateRecord( domainId, *newRecord)
+  status, err := client.CreateRecord( domainId, *newRecord)
   if (err != nil) {
     return fmt.Errorf("Failed to create Rage4 Record: %s", err)
   }
 
   // need to extract record id & save
-  recId := 1
+  recId := status.Id
   d.SetId(string(recId))
-  log.Printf("[INFO] record ID: %s", d.Id())
+  log.Printf("[INFO] created record ID: %s", d.Id())
 
   return nil
 }
 
 func resourceRage4RecordRead(d *schema.ResourceData, meta interface{}) error {
-  // client := meta.(*rage4.Client)
-  return nil
+  // get which record we want to get details on
+  recordId, err := strconv.Atoi(d.Id())
+  domainId, err := strconv.Atoi(d.Get("domainId").(string))
+
+  log.Printf("[INFO] Reading Rage4 Record %d in domain %s", recordId, domainId)
+
+  // get details of records from Rage4
+  client := meta.(*rage4.Client)
+  _, err = client.GetRecords( domainId)
+
+  return err
 }
 
 func resourceRage4RecordUpdate(d *schema.ResourceData, meta interface{}) error {
-  // client := meta.(*rage4.Client)
-  // get id of record to change, 
-  // UpdateRecord( recordId int, record Record) (status Status, err error) {
+  // get which record we want to update
+  recordId, err := strconv.Atoi(d.Id())
+  domainId, err := strconv.Atoi(d.Get("domainId").(string))
 
-  return nil
+  log.Printf("[INFO] Updating Rage4 Record %d in domain %s", recordId, domainId)
+
+  record := new(rage4.Record)
+  record.Name = d.Get("name").(string)
+  record.Content = d.Get("content").(string)
+  record.Type = "A"
+
+  client := meta.(*rage4.Client)
+  _, err = client.UpdateRecord( recordId, *record)
+
+  return err
 }
 
+// delete given resource record for server
 func resourceRage4RecordDelete(d *schema.ResourceData, meta interface{}) error {
   // get which domain we're working on
-  domainId := d.Get("id").(int)
-  recordId := 1
+  recordId, err := strconv.Atoi(d.Id())
+  domainId := d.Get("domainId")
 
-  log.Printf("[INFO] Deleting Rage4 Record for %d in domain %s", d.Get(""), domainId)
+  log.Printf("[INFO] Deleting Rage4 Record %d in domain %s", recordId, domainId)
 
   // ask rage4 to delete record
   client := meta.(*rage4.Client)
-  _,  err := client.DeleteRecord( recordId)
+  _, err = client.DeleteRecord( recordId)
 
   return err
 }
